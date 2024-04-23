7fa4d96420f53d5db0204e240fa5768714ce8afd
Lasith Dissanayake <86358728+DissanayakeLYB@users.noreply.github.com>
2024-04-24 00:44:58 +0530

    Update botLasith.py

--- botLasith.py
+++ botLasith.py
@@ -1,79 +1,86 @@
-#OpenAI key
-OPENAI_API_KEY = #add the key here
+import os
+from dotenv import load_dotenv
 
-#streamlit is for UI
+load_dotenv()
+
+OPENAI_API_KEY = os.getenv("OpenAI_API")
+
+
 import streamlit as st
-
-#to work with PDF,
 from PyPDF2 import PdfReader
-
-#to make chunks
 from langchain.text_splitter import RecursiveCharacterTextSplitter
-
-#to generate embeddings
 from langchain.embeddings.openai import OpenAIEmbeddings
-
-#to call the database 
 from langchain_community.vectorstores.faiss import FAISS
-
-#chaintype load
-from langchain.chains.question_answering import load_qa_chain
-
-#import llm 
+from langchain.chains.history_aware_retriever import create_history_aware_retriever
 from langchain_community.chat_models import ChatOpenAI
 
  
-st.header("Ask Lasith's PDF")
+st.header("PDFTalk")
 
+file = st.file_uploader("Upload a PDF File and start asking questions : ", type="pdf")
 with st.sidebar:
-    st.title("Your Documents")
-    file = st.file_uploader("Upload a PDF File and start asking questions : ", type="pdf")
+    st.title("Chat History")
 
-#extract the text
+
+# Initialize chat history
+if "messages" not in st.session_state:
+    st.session_state.messages = []
+    
+# Display chat messages from history on app rerun
+for message in st.session_state.messages:
+    with st.chat_message(message["role"]):
+        st.write(message["content"])
+
+
 if file is not None:
     pdf_reader = PdfReader(file)
     text = ""
     for page in pdf_reader.pages:
         text += page.extract_text()
-        # st.write(text)
 
-
-    #break it into chunks
     text_splitter = RecursiveCharacterTextSplitter(
         separators = "\n",
-        chunk_size = 500, #these must be updated until you get the best results
-        chunk_overlap = 150, #this allows next chunk to have last 150 tokens of this 1000 words as well. to secure the data 
-        length_function = len
+        chunk_size = 500,
+        chunk_overlap = 150,
     )
 
     chunks = text_splitter.split_text(text)
-    # st.write(chunks)
 
-    #generate embeddings
     embeddings = OpenAIEmbeddings(openai_api_key = OPENAI_API_KEY)
 
-    #creating vectorstore - FAISS
-    #(chunks and embeddings are given as variables as those are the ones that must be stored in the database)
     vector_store = FAISS.from_texts(chunks, embeddings)
 
-    #get user question
-    user_question = st.text_input("Type your question here")
+    user_question = st.chat_input("Ask the question...")
 
-    #do similarity search
+    
+
     if user_question:
-        match = vector_store.similarity_search(user_question) #where is this question stored
-        #st.write(match)
+        match = vector_store.similarity_search(user_question) 
 
-        #define the llm (these values are changed according to the use case - parameter optimization)
         llm = ChatOpenAI(
             openai_api_key = OPENAI_API_KEY,
-            temperature = 0, #lower the value, more specific the model is. ig higher more raqndom information are given
-            max_tokens = 1000,
-            model_tokens = "gpt-3.5-turbo"
+            temperature = 0, 
+            max_tokens = 1000
         )
 
-        #output results
-        chain = load_qa_chain(llm, chain_type = "stuff")
-        response = chain.run(input_documents= match, question = user_question)
-        st.write(response)
+        retriever = vector_store.as_retriever(search_kwargs={"k": 3})
 
+        chain = create_history_aware_retriever(llm = llm, retriever = retriever, prompt = user_question)
+
+        response = chain.run(input_documents= match, question = user_question)
+
+        # # display response of the assistant
+        with st.chat_message("user"):
+            st.write(user_question)
+
+        # add assistant's response to chat history
+        st.session_state.messages.append({"role": "user", "content": user_question})
+
+        response = llm.invoke(user_question)
+
+        # display response of the assistant
+        with st.chat_message("assistant"):
+            st.write(response.content)
+
+        # add assistant's response to chat history
+        st.session_state.messages.append({"role": "assistant", "content": response.content})
